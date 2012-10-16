1c8ff493a908672d3182bcce2010b32ef07c5005
Rob Griffiths <rob@bytespider.eu>
2012-10-16 21:33:49 +0100

    added tests

--- test/resized_test.js
+++ test/resized_test.js
@@ -1,4 +1,5 @@
-var resized = require('../lib/resized.js');
+var fs = require('fs');
+var resized = require('../lib');
 
 /*
   ======== A Handy Little Nodeunit Reference ========
@@ -20,15 +21,167 @@ var resized = require('../lib/resized.js');
     test.ifError(value)
 */
 
-exports['awesome'] = {
-  setUp: function(done) {
-    // setup here
-    done();
-  },
-  'no args': function(test) {
-    test.expect(1);
-    // tests here
-    test.equal(resized.awesome(), 'awesome', 'should be awesome.');
-    test.done();
-  }
+exports.describe = function (test) {
+    test.expect(3);
+
+    var filename = 'original.jpg';
+    var r = resized.describe(filename, function (err, properties) {
+        test.ifError(err);
+
+        test.equal(properties.resolution.width, 1280, 'Width is 1280px');
+        test.equal(properties.resolution.height, 800, 'Height is 800px');
+        test.done();
+    });
+};
+
+exports['Resizing'] = {
+    'width only': function (test) {
+        test.expect(2);
+
+        var filename = 'resized.jpg';
+        resized('original.jpg').resize({width: 400}).write(filename, function () {
+            resized.describe(filename, function (err, properties) {
+                test.ifError(err);
+
+                test.equal(properties.resolution.width, 400, 'Width is 400px after resize');
+                test.done();
+            });
+        });
+    },
+
+    'height only': function (test) {
+        test.expect(2);
+
+        var filename = 'resized.jpg';
+        resized('original.jpg').resize({height: 400}).write(filename, function () {
+            resized.describe(filename, function (err, properties) {
+                test.ifError(err);
+
+                test.equal(properties.resolution.height, 400, 'Height is 400px after resize');
+                test.done();
+            });
+        });
+    },
+
+    'width and height': function (test) {
+        test.expect(3);
+
+        var filename = 'resized.jpg';
+        resized('original.jpg').resize({width: 1000, height: 400}).write(filename, function () {
+            resized.describe(filename, function (err, properties) {
+                test.ifError(err);
+
+                test.equal(properties.resolution.width, 640, 'Width is 640px after resize');
+                test.equal(properties.resolution.height, 400, 'Height is 400px after resize');
+                test.done();
+            });
+        });
+    },
+
+    'width and height ignoring aspec': function (test) {
+        test.expect(3);
+
+        var filename = 'resized.jpg';
+        resized('original.jpg').resize({width: 1000, height: 400, aspect: false}).write(filename, function () {
+            resized.describe(filename, function (err, properties) {
+                test.ifError(err);
+
+                test.equal(properties.resolution.width, 1000, 'Width is 1000px after resize');
+                test.equal(properties.resolution.height, 400, 'Height is 400px after resize');
+                test.done();
+            });
+        });
+    },
+
+    'width and height filling to largest boundery': function (test) {
+        test.expect(3);
+
+        var filename = 'resized.jpg';
+        resized('original.jpg').resize({width: 1000, height: 400, fill: true}).write(filename, function () {
+            resized.describe(filename, function (err, properties) {
+                test.ifError(err);
+
+                test.equal(properties.resolution.width, 1000, 'Width is 1000px after resize');
+                test.equal(properties.resolution.height, 625, 'Height is 625px after resize');
+                test.done();
+            });
+        });
+    }
+};
+
+exports['Cropping'] = {
+    'width only': function (test) {
+        test.expect(3);
+
+        var filename = 'resized.jpg';
+        resized('original.jpg').crop({width: 50}).write(filename, function () {
+            resized.describe(filename, function (err, properties) {
+                test.ifError(err);
+
+                test.equal(properties.resolution.width, 50, 'Width is 50px after resize');
+                test.equal(properties.resolution.height, 800, 'Height is 800px after resize');
+                test.done();
+            });
+        });
+    },
+
+    'height only': function (test) {
+        test.expect(3);
+
+        var filename = 'resized.jpg';
+        resized('original.jpg').crop({height: 50}).write(filename, function () {
+            resized.describe(filename, function (err, properties) {
+                test.ifError(err);
+
+                test.equal(properties.resolution.width, 1280, 'Width is 1280px after resize');
+                test.equal(properties.resolution.height, 50, 'Height is 50px after resize');
+                test.done();
+            });
+        });
+    },
+
+    'width and height': function (test) {
+        test.expect(3);
+
+        var filename = 'resized.jpg';
+        resized('original.jpg').crop({width: 50, height: 50}).write(filename, function () {
+            resized.describe(filename, function (err, properties) {
+                test.ifError(err);
+
+                test.equal(properties.resolution.width, 50, 'Width is 50px after resize');
+                test.equal(properties.resolution.height, 50, 'Height is 50px after resize');
+                test.done();
+            });
+        });
+    },
+
+    'width and height': function (test) {
+        test.expect(3);
+
+        var filename = 'resized.jpg';
+        resized('original.jpg').crop({width: 50, height: 50}).write(filename, function () {
+            resized.describe(filename, function (err, properties) {
+                test.ifError(err);
+
+                test.equal(properties.resolution.width, 50, 'Width is 50px after resize');
+                test.equal(properties.resolution.height, 50, 'Height is 50px after resize');
+                test.done();
+            });
+        });
+    },
+
+    'top left bottom right': function (test) {
+        test.expect(3);
+
+        var filename = 'resized.jpg';
+        resized('original.jpg').crop({top: 50, right: 50, bottom: 50, left: 50}).write(filename, function () {
+            resized.describe(filename, function (err, properties) {
+                test.ifError(err);
+
+                test.equal(properties.resolution.width, 1180, 'Width is 1180px after resize');
+                test.equal(properties.resolution.height, 700, 'Height is 700px after resize');
+                test.done();
+            });
+        });
+    }
 };
